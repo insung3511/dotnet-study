de7c011aa9c5741e80f5f8c7f946fcd9d182c550
insung3511 <insung.park123@gmail.com>
2021-12-29 15:03:40 +0900

    Json read Success

---
+++ 2021Dec29_WriteJsonFromInput/WriteJsonFromInput/Data/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WriteJsonFromInput.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
