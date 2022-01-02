8ce6dcec3c3739e604edfeb83819266eefd99812
insung3511 <insung.park123@gmail.com>
2022-01-02 17:33:43 +0900

    reboot. 2022

---
+++ 2022Jan02_WriteJsonFromInput/WriteJsonFromInput/Data/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WriteJsonFromInput.Data
+{
+    public class Blog
+    {
+        [Required]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogSubTitle { get; set; }
+        public bool BlogHomework { get; set; }
+        //public string BlogContent { get; set; }
+        //public string BlogEditor { get; set; }
+        public DateTime BlogCreatedTime { get; set; }
+    }
+}
---
+++ 2022Jan02_WriteJsonFromInput/WriteJsonFromInput/Data/UploadResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WriteJsonFromInput.Data
+{
+    public class UploadResult
+    {
+        public bool UploadedBool { get; set; }
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        public int ErrorCode { get; set; }
+    }
+}
