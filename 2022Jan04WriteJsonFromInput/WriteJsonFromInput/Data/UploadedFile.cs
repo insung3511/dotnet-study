c20d15e5fbe5034603e18c92e1d2b3f6989acd80
insung3511 <insung.park123@gmail.com>
2022-01-04 04:02:11 +0900

    upload file and input

---
+++ 2022Jan04WriteJsonFromInput/WriteJsonFromInput/Data/UploadedFile.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WriteJsonFromInput.Data
+{
+    public class UploadFile
+    {
+        public string FileName { get; set; }
+        public byte[] FileCount { get; set; }
+    }
+}
