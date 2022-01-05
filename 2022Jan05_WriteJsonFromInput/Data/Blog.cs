b16ad09d84a7cee4993e953ff0d4648e232649a3
insung3511 <insung.park123@gmail.com>
2022-01-05 17:44:18 +0900

    file store success but other prob

---
+++ 2022Jan05_WriteJsonFromInput/Data/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WriteJsonFromInput.Data
+{
+    public class Blog
+    {
+        [Required]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogSubTitle { get; set; }
+        public bool BlogHomework { get; set; }
+        //public string BlogContent { get; set; }
+        //public string BlogEditor { get; set; }
+        public DateTime BlogCreatedTime { get; set; }
+
+        //File Upload Class
+        public bool Uploaded { get; set; }
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        public int? FileCount { get; set; }
+        public int ErrorCode { get; set; }
+    }
+}
